cd6487f30c5b56def6ff9a9791d68aee4b036280
Val Novikov <mail@novweb.ru>
2020-04-24 04:11:54 +0400

    Конфиг для BrowserSync

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,32 @@ config.externals = {
     BX: 'BX'
 };
 
+config.watchOptions = { poll: true, ignored: /node_modules/ };
+
+config.plugins.push(
+  new BrowserSyncPlugin(
+    {
+      host: '0.0.0.0',
+      port: 3000,
+      // proxy: 'localhost',
+      files: [ // watch on changes
+        {
+          match: ['./local/**/*'],
+          fn: function (event, file) {
+            if (event === 'change') {
+              const bs = require('browser-sync').get('bs-webpack-plugin');
+              bs.reload();
+            }
+          }
+        }
+      ],
+    },
+    {
+      reload: false, // this allow webpack server to take care of instead browser sync
+      name: 'bs-webpack-plugin',
+    },
+  )
+);
 
 // export the final configuration
 module.exports = config;
